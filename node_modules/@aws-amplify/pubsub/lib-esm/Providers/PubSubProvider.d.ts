548e527ee651cc57751dba1b52c034c1e376e2c3
Manav Khandurie <manavkhandurie@gmail.com>
2023-04-02 16:05:41 +0530

    newinitaftercommit

---
+++ node_modules/@aws-amplify/pubsub/lib-esm/Providers/PubSubProvider.d.ts
@@ -0,0 +1,14 @@
+import Observable from 'zen-observable-ts';
+import { PubSubProvider, ProviderOptions } from '../types/Provider';
+import { PubSubContent } from '../types/PubSub';
+export declare abstract class AbstractPubSubProvider<T extends ProviderOptions> implements PubSubProvider {
+    private _config;
+    constructor(options: T);
+    configure(config: T): T;
+    getCategory(): string;
+    abstract getProviderName(): string;
+    protected get options(): T;
+    abstract newClient(clientOptions: T): Promise<any>;
+    abstract publish(topics: string[] | string, msg: PubSubContent, options?: T): void;
+    abstract subscribe(topics: string[] | string, options?: T): Observable<PubSubContent>;
+}
