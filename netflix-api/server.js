10f78b86862e5d00ad1bc873c577d910e7355ceb
Manav Khandurie <manavkhandurie@gmail.com>
2023-04-10 00:54:40 +0530

    deploy 11

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -20,17 +20,17 @@ mongoose.connect("mongodb+srv://manavkhandurie:manav123@cluster0.duqmoq7.mongodb
 //app.use("/api/user",userRoutes);
 
 
-app.listen(5000,console.log("server started!!"));
+app.listen(5001,console.log("server started!!"));
 
 const _dirname = path.dirname("");
-console.log(_dirname)
-const buildpath = path.join(_dirname , "../netflix-ui/build");
+console.log(_dirname);
+const buildpath = path.join(_dirname , "../build");
 console.log(buildpath)
 app.use(express.static(buildpath)) 
 app.get("/*", function(req, res){
     console.log("here");
     res.sendFile(
-        path.join(__dirname,"../netflix-ui/build/index.html"),
+        path.join(__dirname,"../build/index.html"),
         
         function(err){
             if(err){
