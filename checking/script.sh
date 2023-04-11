5d2d3a032d6f5ce392122a363cda101fa4a10a63
Manav Khandurie <manavkhandurie@gmail.com>
2023-04-11 10:40:33 +0530

    Bash script changes

--- checking/script.sh
+++ checking/script.sh
@@ -1,21 +1,23 @@
 #!/bin/bash 
 # setting up the server for installing nginx and nodejs
 sudo su
+
 apt update
 
-apt install nginx -y
-
+apt install nginx -y 
+#installing nginx at the server
 curl -s https://deb.nodesource.com/setup_16.x | sudo bash 
-apt install nodejs -y
 
+apt install nodejs -y
+#installing node js at the server
 git clone -b master https://github.com/Manav-Khandurie/AWSNETFLIXSAAS.git
 
 npm install pm2@latest -g
-
+#installing pm2 at the server
 cd AWSNETFLIXSAAS/netflix-api
 
 npm install
-
+#installs dependencies
 cd ..
 
 cd checking
@@ -23,11 +25,12 @@ cd checking
 rm /etc/nginx/sites-available/default
 
 mv default /etc/nginx/sites-available/default
-
+#adds the custom ngnix cong file
 cd ..
 
 cd netflix-api
 
 pm2 start server.js --name netflix 
-
+#starts SAAS application
 sudo service nginx restart
+#restarts the service
