199ad6e2ab8b75d7b762da67c796d2ac81e27898
Manav Khandurie <manavkhandurie@gmail.com>
2023-04-23 15:42:34 +0530

    mk2

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -1,22 +1,18 @@
 const express =require('express');
 const cors=require("cors");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const dynamo=require('dynamodb');
 const userRoutes = require("./routes/UserRoute");
 const path=require("path");
+require('aws-sdk/lib/maintenance_mode_message').suppress = true;
 
+"use strict";
 
 const app=express();
-
+dynamo.AWS.config.update({accessKeyId:process.env.accessKeyId, secretAccessKey: process.env.secretAccessKey, region: process.env.region},()=>console.log("Connected"));
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://manavkhandurie:manav123@cluster0.duqmoq7.mongodb.net/?retryWrites=true&w=majority",{
-    useNewUrlParser : true,
-    useUnifiedTopology : true,
-}).then(()=>{
-    console.log("DB Connected");
-}
-);
 //app.use("/api/user",userRoutes);
 
 
