b07adfb9c8105d62cef391ebced0d1671a9a1e05
Manav-Khandurie <123000337+Manav-Khandurie@users.noreply.github.com>
2023-04-24 11:33:11 +0530

    Update server.js

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -10,13 +10,7 @@ const app=express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://manavkhandurie:manav123@cluster0.duqmoq7.mongodb.net/?retryWrites=true&w=majority",{
-    useNewUrlParser : true,
-    useUnifiedTopology : true,
-}).then(()=>{
-    console.log("DB Connected");
-}
-);
+
 //app.use("/api/user",userRoutes);
 
 
